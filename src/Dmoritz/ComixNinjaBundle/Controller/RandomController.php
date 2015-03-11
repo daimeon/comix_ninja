3f2226926f22f7b27866531d4a80670d4634a379
dmoritz <daniel.moritz@trivago.com>
2015-03-11 16:12:17 +0100

    still following the tutorial

--- src/Dmoritz/ComixNinjaBundle/Controller/RandomController.php
+++ src/Dmoritz/ComixNinjaBundle/Controller/RandomController.php
@@ -2,13 +2,17 @@
 
 namespace Dmoritz\ComixNinjaBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FramworkBundle\Controller\Controller;
 
-class RandomController
+class RandomController extends Controller
 {
-public function indexAction($limit) {
-  return new Response(
-    '<html><body>Number: ' . rand(1, $limit) . '</body></html>'
-  );
-}
+  public function indexAction($limit)
+  {
+    $number = rand(1, $limit);
+
+    return $this->render(
+        'DmoritzComixNinjaBundle:Random:index.html.twig',
+        array('number' => $number)
+    );
+  }
 }

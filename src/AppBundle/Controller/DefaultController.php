056bb563e576acfd2585be84bbae26adfb257d3e
dmoritz <daniel.moritz@trivago.com>
2015-03-11 17:34:52 +0100

    Tut

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Dmoritz\ComixNinjaBundle\Entity\Publisher;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +15,20 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    public function createAction()
+    {
+        $oPublisher = new Publisher();
+        $oPublisher->setName('Ideenschmiede Paul & Paul (IPP)');
+        $oPublisher->setFoundingYear(1999);
+        $oPublisher->setDefunctYear(null);
+        $oPublisher->setCountry('DE');
+
+        $em = $this->getDoctrine()->getManager;
+        $em->persist($oPublisher);
+        $em->flush();
+
+        return new Response('Created publisher id ' . $oPublisher->getId());
     }
 }

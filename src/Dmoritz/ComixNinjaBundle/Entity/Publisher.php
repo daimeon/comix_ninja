57b91fd0e3b32afd90270a3d6a5188052442365a
daimeon <dmoritz79@gmail.com>
2015-03-11 17:17:48 +0100

    tut

---
+++ src/Dmoritz/ComixNinjaBundle/Entity/Publisher.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Dmoritz\ComixNinjaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Publisher
+ */
+class Publisher
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var integer
+     */
+    private $foundingYear;
+
+    /**
+     * @var integer
+     */
+    private $defunctYear;
+
+    /**
+     * @var string
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Publisher
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set foundingYear
+     *
+     * @param integer $foundingYear
+     * @return Publisher
+     */
+    public function setFoundingYear($foundingYear)
+    {
+        $this->foundingYear = $foundingYear;
+
+        return $this;
+    }
+
+    /**
+     * Get foundingYear
+     *
+     * @return integer 
+     */
+    public function getFoundingYear()
+    {
+        return $this->foundingYear;
+    }
+
+    /**
+     * Set defunctYear
+     *
+     * @param integer $defunctYear
+     * @return Publisher
+     */
+    public function setDefunctYear($defunctYear)
+    {
+        $this->defunctYear = $defunctYear;
+
+        return $this;
+    }
+
+    /**
+     * Get defunctYear
+     *
+     * @return integer 
+     */
+    public function getDefunctYear()
+    {
+        return $this->defunctYear;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return Publisher
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}

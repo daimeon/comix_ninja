bf6d5ca3b4c1bfcc7494e962e5d05507cb50e10a
dmoritz <daniel.moritz@trivago.com>
2015-03-11 17:13:21 +0100

    Tut

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmoritz
+ * Date: 11.03.15
+ * Time: 17:12
+ */
+
+namespace AppBundle\Entity;
+
+class Product
+{
+    protected $name;
+    protected $price;
+    protected $description;
+}

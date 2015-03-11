8856edaaf6c780493f940387d87a4c245106209b
Daniel Moritz <dmoritz79@gmail.com>
2015-03-11 13:41:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+comix.ninja
+===========
+
+A Symfony project created on March 11, 2015, 10:18 am.
---
+++ src/Dmoritz/ComixNinjaBundle/DmoritzComixNinjaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dmoritz\ComixNinjaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DmoritzComixNinjaBundle extends Bundle
+{
+}

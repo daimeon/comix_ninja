ed81f1045a85f112915a2ff85a5a518cc811a98d
dmoritz <daniel.moritz@trivago.com>
2015-03-13 14:02:55 +0100

    Tut

--- web/test.php
+++ web/test.php
@@ -10,18 +10,18 @@
     <link href="bootstrap.css" rel="stylesheet">
   </head>
   <body>
-
+  <div class="container">
     <h1>Hello, world!</h1>
-    <div class="container">
-        <div class="row">
-            <div class="col-*-*"></div>
-        </div>
-        <div class="row">
-            <div class="col-*-*"></div>
-            <div class="col-*-*"></div>
-            <div class="col-*-*"></div>
-        </div>
 
+        <div class="row">
+            <div class="col-sm-6" style="background-color:lavender;">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div class="col-sm-6" style="background-color:lavenderblush;">
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            </div>
+        </div>
     </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>

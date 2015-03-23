5e613061b78ee6ec093e421957adce3496d7ee82
dmoritz <daniel.moritz@trivago.com>
2015-03-23 13:35:42 +0100

    basics

--- src/Dmoritz/ComixNinjaBundle/Service/Publisher/PublisherService.php
+++ src/Dmoritz/ComixNinjaBundle/Service/Publisher/PublisherService.php
@@ -8,10 +8,17 @@
 
 namespace Dmoritz\ComixNinjaBundle\Service\Publisher;
 
+use Dmoritz\ComixNinjaBundle\Component\Db\Query\databaseConnect;
+use Dmoritz\ComixNinjaBundle\Component\Db\Query\getPublisher;
+
 class PublisherService
 {
     public function getPublisher($iPublisherId = null)
     {
-        return array('DC', 'Marvel', 'IDW');
+        $_oConnection = new databaseConnect($this->container);
+        $_oPublisherQuery = new getPublisher();
+        $_aPublishers = $_oPublisherQuery->getPublisher();
+
+        return $_aPublishers;
     }
 }

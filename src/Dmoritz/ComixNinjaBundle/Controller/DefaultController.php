9b1bffac5396b8fab3b2d4edd4fe78f7ed63141f
dmoritz <daniel.moritz@trivago.com>
2015-03-24 09:39:55 +0100

    basics#

--- src/Dmoritz/ComixNinjaBundle/Controller/DefaultController.php
+++ src/Dmoritz/ComixNinjaBundle/Controller/DefaultController.php
@@ -6,11 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('DmoritzComixNinjaBundle:Default:index.html.twig', array('name' => $name));
-    }
-
     /**
      *
      * @param $id

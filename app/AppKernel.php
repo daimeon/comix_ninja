a66022484682a8da37a4b3201ef8f2f382d4ff71
daimeon <dmoritz79@gmail.com>
2015-07-20 16:29:10 +0200

    new bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Dmoritz\ComixNinjaBundle\DmoritzComixNinjaBundle(),
+	    new Dmoritz\ComixNinjaApiBundle\DmoritzComixNinjaApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

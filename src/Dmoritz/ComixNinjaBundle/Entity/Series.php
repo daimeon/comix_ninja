a65d8dbb19be0d6e4de4da6b82a7c9d8ada4ff5b
daimeon <dmoritz79@gmail.com>
2015-12-11 13:14:17 +0100

    making series a real entity

--- src/Dmoritz/ComixNinjaBundle/Entity/Series.php
+++ src/Dmoritz/ComixNinjaBundle/Entity/Series.php
@@ -24,4 +24,73 @@ class Series
 	 * @var integer
 	 */
 	public $publisher_id;
+
+    /**
+     * Set series_id
+     *
+     * @param integer $seriesId
+     * @return Series
+     */
+    public function setSeriesId($seriesId)
+    {
+        $this->series_id = $seriesId;
+
+        return $this;
+    }
+
+    /**
+     * Get series_id
+     *
+     * @return integer 
+     */
+    public function getSeriesId()
+    {
+        return $this->series_id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Series
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set publisher_id
+     *
+     * @param integer $publisherId
+     * @return Series
+     */
+    public function setPublisherId($publisherId)
+    {
+        $this->publisher_id = $publisherId;
+
+        return $this;
+    }
+
+    /**
+     * Get publisher_id
+     *
+     * @return integer 
+     */
+    public function getPublisherId()
+    {
+        return $this->publisher_id;
+    }
 }
